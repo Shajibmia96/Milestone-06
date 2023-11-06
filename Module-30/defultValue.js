f1a16d73f948c280ed7338047edd4b51e83af4c2
Shajib96 <shajibahamedornob@gmail.com>
2023-11-06 21:39:11 +0600

    use sequence tag

--- Module-30/defultValue.js
+++ Module-30/defultValue.js
@@ -1,7 +1,21 @@
-function add (num1 , num2){
-            sum = num1+num2;
-            console.log(num1 , num2 , sum)
-            return sum;
-}
+// function add (num1=3 , num2 = 2){
+            // sum = num1+num2;
+            // console.log(num1 , num2 , sum)
+//             return sum;
+// }
 
-add(34 , 21)
+// add( 8)
+
+function fullName (firstName ,  lastName){
+       fullName = firstName + " "+ lastName;
+    //    console.log(fullName);
+       return fullName;
+};
+  fullName( "Shajib" , "Sarkar");
+
+  const a  = 30;
+  const b = 20;
+  const sum = "The sum "  + a +   " and "  +b+  " Total "+  (a+b);
+  console.log(sum);
+  const sequence = `The sum ${a} and  ${b} is equals to ${a+b}`;
+  console.log(sequence)
