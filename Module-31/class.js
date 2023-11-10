91f7ae315e96bef5e71995ae7793803d263e5772
Shajib96 <shajibahamedornob@gmail.com>
2023-11-10 21:00:11 +0600

    added Inheritances Class pototype

---
+++ Module-31/class.js
@@ -0,0 +1,12 @@
+class Teacher{
+  constructor(name , sub , id){
+            this.name = name;
+            this.sub = sub;
+            this.id = id;
+  }
+}
+
+const shohidul = new Teacher("Shahidul" , "Mathematics" , 43506344);
+const arshed = new Teacher("Arshed" , "English" , 853983720);
+console.log(arshed)
+console.log(shohidul)
---
+++ Module-31/inheritannceClass.js
@@ -0,0 +1,36 @@
+class Address {
+    constructor(district , policeStation, union){
+         this.district = district;
+         this.policeStation = policeStation ; 
+         this.union = union;
+
+    }
+    occupation(){
+        console.log("He is a student")
+    }
+};
+
+
+class HouseNumber extends Address {
+     constructor(district , policeStation, union ,village,aria , houseNumber){
+        super(district , policeStation ,union);
+        this.village = village
+        this.aria = aria
+        this.houseNumber=houseNumber
+     }
+} ;
+
+
+ class FamilyMember extends HouseNumber{
+    constructor(district , policeStation, union ,village,aria , houseNumber,FamilyMember){
+        super(district , policeStation, union ,village,aria , houseNumber)
+         this.FamilyMember = FamilyMember
+    }
+ } 
+
+const Shajib = new HouseNumber("Narayangonj" , "Araihajar" , "Bishnondi", "Chaitankanda" , "Uttor para  Sarkar Bari" , 596);
+const Sayem = new HouseNumber ("Narsingdi","Raipura" ,"Adiabad" , "ShirajNogorNoyachor","East para","KadirBari", 322 );
+const Akash = new FamilyMember("Narsingdi" , "Ship por" , "Ushor" , "Sristigor", "Hajibagan", 122 , 4 );
+console.log(Akash)
+// console.log(Sayem)
+// console.log(Shajib)
