05b6574a303ef295a1d08992efcda4f61afcba09
Shajib96 <shajibahamedornob@gmail.com>
2023-11-11 13:41:38 +0600

    find different value is truthy or falsy

---
+++ Module-32/truthy.js
@@ -0,0 +1,12 @@
+const x ={name:"Akkhas"};
+if(x){
+   console.log("This value is truthy")
+}else{
+    console.log("this value is Falsy")
+}
+
+
+const y = {age:43};
+if(!!y){
+    console.log("this value is truthy $")
+}
